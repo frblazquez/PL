6b20b4f150aa07998da109747c53c4d530174549
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2020-04-17 13:36:37 +0200

    Practice folder reorganization

--- prueba_especificacion_practica/cup.sh
+++ prueba_especificacion_practica/cup.sh
@@ -1,11 +1,16 @@
+# Script for the automatic generation of the lexical analyzer. Run this every 
+# time sintactical specification (src/asint/AnalizadorSintacticoTiny.cup) is modified.
+
 java -jar cup.jar -parser AnalizadorSintacticoTiny -symbols ClaseLexica -nopositions src/asint/Tiny.cup
 
 FILE1=AnalizadorSintacticoTiny.java
 FILE2=ClaseLexica.java
 
 if test -f $FILE1; then
-	rm src/asint/$FILE1
-	rm src/asint/$FILE2
+    # mv will automatically override old files, commented to avoid error msgs if no file is present
+    # rm src/asint/$FILE1
+	# rm src/asint/$FILE2
 	mv $FILE1 src/asint/$FILE1
 	mv $FILE2 src/asint/$FILE2
 fi
+
--- prueba_especificacion_practica/jlex.sh
+++ prueba_especificacion_practica/jlex.sh
@@ -1,4 +1,7 @@
-java JLex.Main src/alex/AnalizadorLexicoTiny.l
+# Script for the automatic generation of the lexical analyzer. Run this every 
+# time lexical specification (src/alex/AnalizadorLexicoTiny.l) is modified.
+
+java -cp jlex.jar JLex.Main src/alex/AnalizadorLexicoTiny.l
 
 NFILE=src/alex/AnalizadorLexicoTiny.l.java
 OFILE=src/alex/AnalizadorLexicoTiny.java
@@ -6,3 +9,4 @@ OFILE=src/alex/AnalizadorLexicoTiny.java
 if test -f $NFILE; then
 	mv $NFILE $OFILE
 fi
+
