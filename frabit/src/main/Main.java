328cb8e1277479785f9f0389c900364290b82a1a
Francisco Javier Blazquez Martinez <fcojavibm1@gmail.com>
2020-05-31 10:26:09 +0200

    Test files reorganization

--- frabit/src/main/Main.java
+++ frabit/src/main/Main.java
@@ -12,13 +12,21 @@ public class Main {
 
     //@formatter:off
     public static final String BASE_FOLDER 		 = "test-files/";
-    public static final String TEST_ALL 		 = "input.txt";
-    public static final String TEST_INSTRUCTIONS_DISPLAY = "instructions.txt";
-    public static final String TEST_ACCESSES_DISPLAY 	 = "accesses.txt";
-    public static final String TEST_EXPRESSIONS_DISPLAY  = "expressions.txt";
-    public static final String TEST_TYPES_DISPLAY 	 = "types.txt";
-    public static final String TEST_ERROR_RECOVERY = "error_recovery.txt";
-    public static final String TEST_SEMANTIC_ERRORS = "sem_errors.txt";
+    public static final String AST_DISPLAY_FOLDER        = BASE_FOLDER+"ast/";
+    public static final String ERROR_RECOVERY_FOLDER     = BASE_FOLDER+"error-recovery/";
+    public static final String SEMANTIC_ANALYSIS_FOLDER  = BASE_FOLDER+"sem-analysis/";
+    public static final String TEST_ALL 		 = BASE_FOLDER+"input.txt";
+    
+    public static final String TEST_INSTRUCTIONS_DISPLAY = "instructions_ast.txt";
+    public static final String TEST_ACCESSES_DISPLAY 	 = "accesses_ast.txt";
+    public static final String TEST_EXPRESSIONS_DISPLAY  = "expressions_ast.txt";
+    public static final String TEST_TYPES_DISPLAY 	 = "types_ast.txt";
+    public static final String TEST_ERROR_RECOVERY  	 = "error_recovery.txt";
+    public static final String TEST_TYPE_ERROR_RECOVERY  = "types_er.txt";
+    public static final String TEST_EXP_ERROR_RECOVERY   = "expressions_er.txt";
+    public static final String TEST_INST_ERROR_RECOVERY  = "instructions_er.txt";
+    public static final String TEST_COMMON_ERROR_RECOVERY= "common_er.txt";
+    public static final String TEST_SEMANTIC_ERRORS 	 = "errors_sa.txt";
     //@formatter:on
 
     public static void main(String[] args) throws Exception {
@@ -27,6 +35,8 @@ public class Main {
 	Reader input = new InputStreamReader(new FileInputStream(input_file));
 	AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
 	AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(alex);
+
+	// TODO: Why this try-catch?
 	try
 	{
 		Program pr = (Program) asint.parse().value;
